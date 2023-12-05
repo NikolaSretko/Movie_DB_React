6b12f7da9013f075974f2380e0a0baf12fad98ba
NikolaSretko <nikola.sretkovic99@gmail.com>
2023-12-05 20:49:05 +0100

    clear

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,26 +5,23 @@ import movies from "../data/movies";
 import './Home.scss'
 
 const Home = () => {
-    // Zustand für die gesamte Filmliste
     const [filmArr, setFilmArr] = useState(movies);
-    // Zustand für die gefilterte Filmliste, die durch Sortier- oder Suchfunktionen aktualisiert wird
+
     const [filteredFilmArr, setFilteredFilmArr] = useState(movies);
   
-    // Funktion für die Sortierung der Filme
+
     const sortBy = (sortFunction) => {
-      // Erstelle eine Kopie der Filmliste und wende die übergebene Sortierfunktion an
+
       const sortedFilms = [...filmArr].sort(sortFunction);
-      // Aktualisiere den Zustand für die gefilterte Filmliste
       setFilteredFilmArr(sortedFilms);
     };
   
-    // Funktion für die Suche nach Filmen basierend auf einem Suchbegriff
+
     const searchFunction = (searchTerm) => {
-      // Filtere die Filmliste nach Titeln, die den Suchbegriff enthalten
+
       const filteredFilms = filmArr.filter((film) =>
         film.title.toLowerCase().includes(searchTerm.toLowerCase())
       );
-      // Aktualisiere den Zustand für die gefilterte Filmliste
       setFilteredFilmArr(filteredFilms);
     };
   
@@ -40,7 +37,6 @@ const Home = () => {
           filterByZA={() => sortBy((a, b) => b.title.localeCompare(a.title))}
           searchFunction={searchFunction}
         />
-        {/* Übergebe die gefilterte Filmliste an die FilmList-Komponente */}
         <FilmList filmArr={filteredFilmArr} />
       </section>
     );
